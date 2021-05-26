5ba56473b93e961fa1b878e947e47cc13efdd8af
kortres <leonchudin@mail.ru>
2021-05-27 00:25:08 +0300

    Test version 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'help' => 'static_pages#help'
   get 'signup'  => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
