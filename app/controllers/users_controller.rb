ee359c508dfadf7ee1d88ab6102170bf0502f3a8
kortres <leonchudin@mail.ru>
2021-05-27 01:49:47 +0300

    It'is work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index]
+  before_action :logged_in_user, only: [:index, :destroy]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.all
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
   def show
