58b334e37d5c2e59e720ae87869f348671b38d06
kortres <leonchudin@mail.ru>
2021-05-29 11:43:56 +0300

    Mini fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :edit, :show, :index, :destroy]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:destroy, :edit]
   before_action :set_post, only: %i[ show edit update destroy ]
 
   def admin_user
